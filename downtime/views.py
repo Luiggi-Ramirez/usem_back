d00b2d7038ceb8f0188da1474e757af726eebbf2
MarocLV <marco_321vergara@hotmail.com>
2022-09-22 09:42:33 -0600

    added total line down time handler & code ref.

--- downtime/views.py
+++ downtime/views.py
@@ -1,3 +1,5 @@
+from tracemalloc import start
+from urllib import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,6 +25,9 @@ class DownTimeView(APIView):
         queryset = DowntimeDetails.objects.all()
 
         line_number = self.request.query_params.get('line_number')
+        from_date = self.request.query_params.get('from_date')
+        to_date = self.request.query_params.get('to_date')
+        total_line_dt = self. request.query_params.get('total_line_dt')
 
         '''line_number is a mandatory query param'''
         if not line_number: 
@@ -30,28 +35,54 @@ class DownTimeView(APIView):
         else:
             queryset = queryset.filter(line_number=line_number)
 
-        qset_dict = queryset.values()
-
-
-        start_time = []
-        end_time = []
-        for i in range(len(qset_dict)):
-            '''Access to the attributes "start"-"end" from all the objects in the query results'''
-            start = qset_dict[i]['start']
-            end = qset_dict[i]['end']
-            start_time.append(start.strftime("%X").split(":"))
-            end_time.append(end.strftime("%X").split(":"))
-
-          
-        serializer= DowntimeDetailsSerializer(queryset,many=True)
-        serialized_data = serializer.data
-
-        for i in range(len(start_time)) and range(len(end_time)):
+        '''filter by a range of dates'''
+        if from_date or to_date:
             
-            result = timedelta(hours=int(end_time[i][0]), minutes=int(end_time[i][1])) - timedelta(hours=int(start_time[i][0]), minutes=int(start_time[i][1]))
-            serialized_data[i]['downtime'] = result / 60
+            queryset = queryset.filter(date__range=[from_date, to_date])
+        
+        '''No-data handler'''
+        if not queryset:
+            
+            return Response({"message" : "no data"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+
+            qset_dict = queryset.values() #transform query in a dict
+
+            serializer= DowntimeDetailsSerializer(queryset,many=True)
+            serialized_data = serializer.data
+
+            
+            total_dt = []
+            total_res = {}
+            for i in range(len(qset_dict)):
+                '''Access to the attributes "start"-"end" from all the objects in the query results'''
+                start = qset_dict[i]['start']
+                end = qset_dict[i]['end']
+
+                # calculation to get the difference time
+                result = timedelta(hours=end.hour, minutes=end.minute) - timedelta(hours=start.hour, minutes=start.minute)
+
+                '''convert the result in minutes and save it in a new property called
+                "downtime" in the serializer'''
+                serialized_data[i]['downtime'] = round(result.total_seconds() / 60)
+
+                '''convert the result in minutes and save it in the list
+                 "total_dt" to add all the results and get the total down time'''
+                total_dt.append(round(result.total_seconds() / 60))
+
+                #get the name of the line to save it as a property in the json that will be sent as a response
+                total_res["line"] = serialized_data[i]["line_name"]
+                
+
+            total_downtime = sum(total_dt) # sum of all the results to obtain the total down time
+
+            # Save the total down time in the json that will be sent as a response
+            total_res["total_downtime"] = total_downtime 
+        
+            if total_line_dt == 'true':
+            
+                return Response(total_res, status=status.HTTP_200_OK)
             
 
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
+            return Response(serialized_data, status=status.HTTP_200_OK)
 
