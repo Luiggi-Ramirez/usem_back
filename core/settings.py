e5765ff21f86aa2c510ea323d007c86567fc4677
MarocLV <marco_321vergara@hotmail.com>
2022-10-28 12:14:18 -0600

    sql-server connection configuration

--- core/settings.py
+++ core/settings.py
@@ -74,10 +74,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'usemdb',
+        'USER': 'sa',
+        'PASSWORD': 'admin1234',
+        'HOST': 'DESKTOP-5MJ7PNA',
+        'PORT': '',
+
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server'
+        },
     }
 }
 
