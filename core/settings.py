272a58b4af5b5b2e43b970ebf5289e04eb8ef3b4
Ubuntu <ubuntu@ip-172-31-95-31.ec2.internal>
2022-11-03 22:12:01 +0000

    Agregado django corsheaders

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['kpi.syncronik.com', '*']
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -26,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+   
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
